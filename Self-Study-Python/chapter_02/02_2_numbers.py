1e8ea759a9756c4b039521d46a41236b6285e423
hoonvas <jihun96700@gmail.com>
2025-11-10 13:43:05 +0000

    chapter_02_2

---
+++ Self-Study-Python/chapter_02/02_2_numbers.py
@@ -0,0 +1,5 @@
+# 숫자의 종류
+print(273)
+print(52.273)
+print(type(273))
+print(type(52.273))
---
+++ Self-Study-Python/chapter_02/02_2_operator_precedence.py
@@ -0,0 +1,8 @@
+# 연산자 우선순위
+print(2 + 2 - 2 * 2 / 2 * 2)
+print(2 - 2 + 2 / 2 * 2 + 2)
+
+# TypeError 예외
+string = "문자열"
+number = 273
+print(string + number)
