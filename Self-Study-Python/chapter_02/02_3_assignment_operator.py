77b4245b453b125a784ca22f35e532de62e60aec
hoonvas <jihun96700@gmail.com>
2025-11-11 13:48:38 +0000

    chapter_02_3

---
+++ Self-Study-Python/chapter_02/02_3_assignment_operator.py
@@ -0,0 +1,12 @@
+# 숫자열 복합 대입 연산자
+number = 100
+number += 10
+number += 20
+number += 30
+print("number:", number)
+
+# 문자열 복합 대입 연산자
+string = "안녕하세요"
+string += "!"
+string += "!"
+print("string:", string)
---
+++ Self-Study-Python/chapter_02/02_3_inputs.py
@@ -0,0 +1,10 @@
+string = input("인사말을 입력하세요> ")
+print(string)
+
+# input() 함수의 입력 자료형
+print(type(string))
+
+number = input("숫자를 입력하세요> ")
+print(number)
+
+print(type(number))
